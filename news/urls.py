991a91a72269e9f0326920b4bbbdf36608104015
skgx <saurabhgupta2131@gmail.com>
2022-09-22 20:05:34 +0530

    deleted unessary codes

--- news/urls.py
+++ news/urls.py
@@ -2,9 +2,6 @@ from django.contrib import admin
 from django.urls import path,include
 from news import views
 urlpatterns = [
-   #path('admin/',admin.site.urls),
     path('',views.index,name="home"),
-    # path('about',views.about,name="about"),
-    # path('services',views.services,name="services"),
-    # path('contact',views.contact,name="contact"),
+    
 ]
