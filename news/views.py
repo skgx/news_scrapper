bd535aff3b658377441ca94fc052eca32cb6d51d
skgx <saurabhgupta2131@gmail.com>
2023-02-05 12:47:54 +0530

    file update

--- news/views.py
+++ news/views.py
@@ -1,8 +1,6 @@
-
 import requests
 from django.shortcuts import render
 from bs4 import BeautifulSoup
-
 
 toi_r = requests.get("https://timesofindia.indiatimes.com/briefs")
 toi_soup =BeautifulSoup(toi_r.content, 'html5lib')
@@ -17,7 +15,6 @@ for th in toi_headings:
     if i>2:
       toi_news.append(th.text)
     i=i+1
-
 
 def index(request):
     return render(request, 'index.html', {'toi_news':toi_news})
