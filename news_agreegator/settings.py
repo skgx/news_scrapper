8a565fb0b43524223946988d6a7c8001ec4b3288
skgx <saurabhgupta2131@gmail.com>
2023-02-11 12:19:44 +0530

    deployment

--- news_agreegator/settings.py
+++ news_agreegator/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = 'django-insecure-@tv@#rytnera8@o7=le&5%jn_5i3mv!yyq1ua)0v-hmo$81b4r
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
@@ -110,3 +110,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import os
+STATICFILES_DIRS=os.path.join(BASE_DIR,'static'),
+STATIC_ROOT=os.path.join(BASE_DIR,'staticfiles_build','static')
